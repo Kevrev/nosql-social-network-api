3ba939ffe73a71b2594b6a39c47b2bfb60ceb890
Seer <ksmnk@protonmail.com>
2023-07-14 22:23:20 -0700

    added user model

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const db = require('./config/connection');
 // const routes = require('./routes');
 
+const cwd = process.cwd();
+
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 // app.use(routes);
 
 db.once('open', () => {
